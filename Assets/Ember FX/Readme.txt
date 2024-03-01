0dcdcd7333fb66ec299fa0869bc7267cbfcc286c
KIRCASTUDIOS\kirca <kircastudios@gmail.com>
2024-03-01 23:51:13 +0300

    ember fx

---
+++ Assets/Ember FX/Readme.txt
@@ -0,0 +1,13 @@
+==============
+URP Upgrade
+==============
+
+The Upgrades folder contains a unitypackage that will allow you to upgrade the project to URP. Double-click it and choose Import. To revert to Standard, re-import the asset.
+
+If you have any visual artifacts in URP, make sure you are using the latest version of URP in the Package Manager.
+
+==============
+Support
+==============
+
+archanor.work@gmail.com
---
+++ Assets/Ember FX/Scripts/EmberFXSceneMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace EmbersFX
+{
+
+public class EmberFXSceneMenu : MonoBehaviour
+{
+    public void LoadEmbers01()
+    {
+        SceneManager.LoadScene("Ember01");
+    }
+    public void LoadEmbers02()
+    {
+        SceneManager.LoadScene("Ember02");
+    }
+    public void LoadEmbers03()
+    {
+        SceneManager.LoadScene("Ember03");
+    }
+	public void LoadEmbers04()
+    {
+        SceneManager.LoadScene("Ember04");
+    }
+	public void LoadEmbers05()
+    {
+        SceneManager.LoadScene("Ember05");
+    }
+}
+
+}
